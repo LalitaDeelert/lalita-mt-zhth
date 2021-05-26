d06b4eefb2f0f95a8f92e77a71b14df6f48ead76
cstorm125 <cebril@gmail.com>
2021-05-26 15:42:21 +0700

    add script template for clean_data.py

---
+++ scripts/clean_data.py
@@ -0,0 +1,27 @@
+import argparse
+from pythainlp.tokenize import word_tokenize
+import jieba
+
+def main(args):
+    #function to clean data containing sentence pairs 
+    print(args)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_fname', type=str, 
+                       help='Input file containing sentence pairs to clean')
+    parser.add_argument('--output_fname', type=str, 
+                       help='File to save as output')
+    parser.add_argument('--lowercase', action='store_true',
+                        help='Lowercase all characters')
+    parser.add_argument('--min_similarity', default=0.5,
+                        type=float, help='Minimum mUSE similarity score to retain')
+    parser.add_argument('--min_zhth_ratio', default=0.2,
+                        type=float, help='Minimum ZH/TH word ratio to retain')
+    parser.add_argument('--max_zhth_ratio', default=2,
+                        type=float, help='Maximum ZH/TH word ratio to retain')
+
+    args = parser.parse_args()
+    main(args)
+    
